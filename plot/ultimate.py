a37916bc13a2adebd7ccac92f9579f0248597072
Jacob Downs <jacob.downs@umontana.edu>
2019-02-11 17:18:17 -0700

    done with buizert runs

--- plot/ultimate.py
+++ plot/ultimate.py
@@ -11,13 +11,14 @@ matplotlib.rcParams.update({'font.size': 18})
 
 fig = plt.figure(figsize=(12,13))
 ksmooth = signal.hann(11) / signal.hann(11).sum()
-start = -9.5
+start = -11.6
 
 
 ### Buizert temp.
 ############################################################
 
 ax = fig.add_subplot(3,1,1)
+ax.axvspan(-10., -8., alpha=0.33, color='gray')
 current_palette = sns.color_palette("coolwarm", 12)
 
 indexes = [0, 1, 2, 3, 4, 5, 6,  7,  8, 9, 10, 11]
@@ -25,9 +26,18 @@ indexes = [0, 2, 4, 7, 8, 9, 10, 8, 6, 5, 3,  1]
 
 dt_years = np.loadtxt('../paleo_data/buizert_ages.txt') / 1e3
 dt_vals = np.loadtxt('../paleo_data/buizert_dts.txt').T
-
 dt_w = [0, 1, 2]
 
+dt_dj = np.loadtxt('../paleo_data/jensen_dye3.txt')
+ages_dj = (dt_dj[:,0] - 1950.)
+dt_dj = dt_dj[:,1]
+dt_dj -= dt_dj[-1]
+dt_dj = dt_dj[:, None].repeat(12, axis=1).T
+
+"""
+np.savetxt('../paleo_data/jensen_ages.txt', ages_dj)
+np.savetxt('../paleo_data/jensen_dts.txt', dt_dj)
+"""
 
 dt_avg = np.zeros(len(dt_years))
 for i in range(12):
@@ -39,9 +49,12 @@ for i in range(12):
 dt_smooth = signal.convolve(dt_vals[i], ksmooth, mode = 'same')
 #plt.plot(dt_years, dt_smooth, color = 'w', lw = 5.5)
 plt.plot(dt_years, dt_smooth, color = 'k', lw = 3)
+plt.plot(ages_dj, dt_dj, color = 'k', lw = 3)
+
+
 
 plt.xlim([start, 0.])
-plt.ylim([-6., 3.5])
+plt.ylim([-10., 5.])
 plt.grid(color='slategray', linestyle=':', linewidth=1)
 plt.ylabel(r'$\Delta T$ ($^{\circ}$ C)')
 
@@ -50,6 +63,7 @@ plt.ylabel(r'$\Delta T$ ($^{\circ}$ C)')
 ##########################################################
 
 ax = fig.add_subplot(3,1,2)
+ax.axvspan(-10., -8., alpha=0.33, color='gray')
 current_palette =  sns.color_palette()
 
 data1 = DataLoader('../transform_final/center3/', 'opt1/')
@@ -80,9 +94,9 @@ plt.plot(data2.sigma_ages, data2.deltap, color = current_palette[3], marker = 'o
 plt.plot(data2.ages, data2.precip, color = 'k', lw = 3.)
 plt.plot(data2.ages, data2.precip, color = current_palette[3], lw = 2.)
 
-plt.plot(data3.sigma_ages, data3.deltap, color = 'k', marker = 'o', lw = 3.5, ms=8)
-plt.plot(data3.sigma_ages, data3.deltap, color = 'k', marker = 'o', lw = 2.25, ms=6, label = 'North')
-plt.fill_between(data3.sigma_ages, data3.deltap_l, data3.deltap_u, color = 'gray', alpha = 0.75)
+#plt.plot(data3.sigma_ages, data3.deltap, color = 'k', marker = 'o', lw = 3.5, ms=8)
+#plt.plot(data3.sigma_ages, data3.deltap, color = 'k', marker = 'o', lw = 2.25, ms=6, label = 'North')
+#plt.fill_between(data3.sigma_ages, data3.deltap_l, data3.deltap_u, color = 'gray', alpha = 0.75)
 
 plt.ylim([-0.05, 0.65])
 plt.xlim([start, 0.])
@@ -94,19 +108,33 @@ plt.ylabel(r'$\Delta P$ (m.w.e. a$^{-1}$)')
 ### Glacier lengths
 ##########################################################
 
-plt.subplot(3,1,3)
+ax = plt.subplot(3,1,3)
+ax.axvspan(-10., -8., alpha=0.33, color='gray')
+
+# Center 
+L1_obs = np.array([406878, 396313, 321224, 292845, 288562, 279753]) / 1e3
+# south
+L2_obs  = np.array([424777, 394942, 332430, 303738, 296659, 284686]) / 1e3
+# Observation ages
+obs_ages = np.array([-11554., -10284., -9024., -8064., -7234., 0.]) / 1e3
+# Observation variances
+obs_sigmas = np.array([0.4, 0.2, 0.2, 0.3, 0.3, 0.1])/2.
+
 plt.plot(data1.ages, data1.L, color = 'k', lw = 5)
-plt.plot(data1.ages, data1.L, color = current_palette[0], lw = 4)
+plt.plot(data1.ages, data1.L, color = current_palette[0], lw = 4, label = 'North')
 
 plt.plot(data2.ages, data2.L, color = 'k', lw = 5)
-plt.plot(data2.ages, data2.L, color = current_palette[3], lw = 4)
+plt.plot(data2.ages, data2.L, color = current_palette[3], lw = 4, label = 'South')
+
 
 plt.ylim([275, 360])
 plt.xlim([start, 0.])
 plt.grid(color='slategray', linestyle=':', linewidth=1)
 
+
 plt.ylabel('Glacier Length (km)')
 plt.xlabel('Age (ka BP)')
+plt.legend()
 
 plt.tight_layout()
 
