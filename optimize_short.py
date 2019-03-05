3bebdb76548980c2b8ddda74d526c5f64405a8f3
Jacob Downs <jacob.downs@umontana.edu>
2019-03-04 20:46:23 -0700

    asdf

---
+++ optimize_short.py
@@ -0,0 +1,19 @@
+import numpy as np
+from stats.optimizer import *
+import sys
+
+# Flowline
+flowline = sys.argv[1]
+# Input dictionary
+inputs = {}
+    
+### Center
+#############################################################
+
+if flowline == 'center1': 
+    inputs['in_dir'] = 'transform_short/center1/'
+    inputs['y_ages'] = np.loadtxt('paleo_inputs/y_ages.txt')
+    inputs['y'] = np.loadtxt('paleo_inputs/y_c.txt')
+    inputs['Py'] = 1.*np.loadtxt('paleo_inputs/Py_c.txt')
+    opt = Optimizer(inputs)
+    opt.optimize(out_dir = 'transform_short/center1/opt1/')
